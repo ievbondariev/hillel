59ef66a9459d884ed7a71573368fb4624bce3624
unknown <iev.bondariev@gmail.com>
2021-02-11 23:10:15 +0200

    task1-3

--- src/lesson5/homework/Task3.java
+++ src/lesson5/homework/Task3.java
@@ -1,5 +1,7 @@
 package lesson5.homework;
 
+import java.util.Scanner;
+
 public class Task3 {
 
     /**
@@ -25,6 +27,7 @@ public class Task3 {
      *
      */
     public static void main(String[] args) {
-        // write your code here
+
+
     }
 }
