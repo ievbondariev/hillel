265c0555cd489d0d167c3768eb55f608dbd80e24
Nadiia Romanushko <romanushkoq@gmail.com>
2021-02-13 22:14:44 +0200

    Task4 and Task5

--- src/lesson5/homework/Task4.java
+++ src/lesson5/homework/Task4.java
@@ -1,4 +1,5 @@
 package lesson5.homework;
+import java.util.Scanner;
 
 public class Task4 {
 
@@ -6,20 +7,43 @@ public class Task4 {
      * Дан массив целых чисел. Если в нем есть два соседних элемента одного знака, выведите эти числа.
      * Если соседних элементов одного знака нет - не выводите ничего.
      * Если таких пар соседей несколько - выведите первую пару.
-     *
+     * <p>
      * Формат входных данных
      * В первой строке вводится количество элементов в массиве. Во второй строке вводятся элементы массива. Все числа отличны от нуля.
      * Формат выходных данных
      * Выведите ответ на задачу. Элементы выводятся в порядке неубывания.
-     *
+     * <p>
      * Sample Input:
      * 5
      * -1 2 3 -1 -2
-     *
+     * <p>
      * Sample Output:
      * 2 3
+     * @return
      */
+
+
     public static void main(String[] args) {
-        // write your code here
-    }
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите кол-во элементов массива:  ");
+        int n = in.nextInt();
+
+
+        int[] a = new int[n];
+        System.out.println("Введите числа");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextInt();
+
+        } for (int i = 1; i < n; i++)
+
+            if (a[i] > 0 && a[i-1] > 0 || a[i] < 0 && a[i-1] < 0){
+
+                System.out.println("Два соседних элемента одного знака: " + a[i-1] + " " + a[i]);
+                break;
+
+            }
+        }
 }
+
+
--- src/lesson5/homework/Task5.java
+++ src/lesson5/homework/Task5.java
@@ -1,5 +1,7 @@
 package lesson5.homework;
 
+import java.util.Arrays;
+import java.util.Scanner;
 public class Task5 {
 
     /**
@@ -7,23 +9,63 @@ public class Task5 {
      * F(0) = 0, F(1) = 1, …, F(n) = F(n−1) + F(n−2).
      * Дано натуральное число A. Определите, каким по счету числом Фибоначчи оно является,
      * то есть выведите такое число N, что F(N) = A. Если А не является числом Фибоначчи, выведите число -1.
-     *
+     * <p>
      * Формат входных данных:
      * Вводится натуральное число A > 1.
-     *
+     * <p>
      * Формат выходных данных:
      * Выведите ответ на задачу.
-     *
+     * <p>
      * Sample Input:
      * 8
-     *
+     * <p>
      * Sample Output:
      * 6
-     *
+     * <p>
      * https://en.wikipedia.org/wiki/Fibonacci_number
      * RU: https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8
      */
-    public static void main(String[] args) {
-        // write your code here
-    }
+
+
+
+        public static void main(String[] args) {
+
+            System.out.println("Введите число больше 0:  ");
+
+            Scanner s = new Scanner(System.in);
+            int n = s.nextInt();
+            if (n <= 0){
+                System.out.println("Фигня твое число, заново!");
+            }
+
+            int i = 0;
+            int a = 1;
+            int b = 0;
+            int sum = 0;
+            for (i = 0; i < n; i++) {
+                int fibb = a + b;
+                a = b;
+                b = fibb;
+                int d = i + 1;
+                sum = d;
+                if (n == fibb) {
+                    System.out.println(d);
+                 break;
+                } if (n <= d) {
+                    System.out.println("-1");
+                   break;
+                }
+            }
+        }
+
 }
+
+
+
+
+
+
+
+
+
+
